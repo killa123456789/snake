e938cefc5bf7cbfc1972d6aa0efd9acd8a03c4e2
killa123456789 <ilya-volchkov@yandex.ru>
2016-07-21 15:39:37 +0300

    Поменял тело змейки

--- src/Main/Sprite.java
+++ src/Main/Sprite.java
@@ -8,7 +8,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public enum Sprite {
-    BODY("circle"), CHERRIES("cherries");
+    BODY("rectangle"), CHERRIES("cherries");
 
     private Texture texture;
 
